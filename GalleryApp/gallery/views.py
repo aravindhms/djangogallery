184e33e5ef8437dbcda0e6169cc8dedd7eb340cb
Aravindh <aravindhms1@gmail.com>
2019-08-24 01:28:03 +0530

    Dynamic image gallery successfully generated
    Success

--- GalleryApp/gallery/views.py
+++ GalleryApp/gallery/views.py
@@ -2,9 +2,19 @@ from django.shortcuts import render
 from django.template import loader
 from django import forms
 import gallery
+from GalleryApp import settings
+import os
 
 # Create your views here.
 def imggallery(request):
+
+    image_list=[]
+    static_dirs = settings.STATICFILES_DIRS
+    for dir in static_dirs:
+        imgpath = os.path.join(dir,'images')
+        for file in os.listdir(imgpath):
+            if file.endswith(".jpg"):
+                image_list.append(settings.STATIC_URL+'images/'+file)
     # return HttpResponse('<h1>Hello</h1>')
     # template = loader.get_template('index.html')
-    return render(request, 'index.html')
+    return render(request, 'gallery.html',context={'image_list':image_list})
