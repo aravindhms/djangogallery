3ecdec0307f5527fd4d606af2b356d6f65eb7019
Aravindh <aravindhms1@gmail.com>
2019-08-23 23:00:30 +0530

    Added first template rendering

--- GalleryApp/gallery/views.py
+++ GalleryApp/gallery/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template import loader
 from django import forms
 import gallery
 
 # Create your views here.
-def imggallery(self):
-    return HttpResponse('<h1>Hello</h1>')
-    pass
+def imggallery(request):
+    # return HttpResponse('<h1>Hello</h1>')
+    # template = loader.get_template('index.html')
+    return render(request, 'index.html')
