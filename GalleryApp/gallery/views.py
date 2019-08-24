0dc1d301854a709440bacad518e0c4c7db0ac77e
Aravindh <aravindhms1@gmail.com>
2019-08-24 10:54:17 +0530

    EXIF data added on image overlay

--- GalleryApp/gallery/views.py
+++ GalleryApp/gallery/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render
 from django.template import loader
 from django import forms
-import gallery
 from GalleryApp import settings
 import os
+import exifread
 
-# Create your views here.
+# Get a list of all '.jpg' files from specified directory
 def imggallery(request):
-
-    image_list=[]
+    allimages={}
     static_dirs = settings.STATICFILES_DIRS
     for dir in static_dirs:
         imgpath = os.path.join(dir,'images')
         for file in os.listdir(imgpath):
             if file.endswith(".jpg"):
-                image_list.append(settings.STATIC_URL+'images/'+file)
-    # return HttpResponse('<h1>Hello</h1>')
-    # template = loader.get_template('index.html')
-    return render(request, 'gallery.html',context={'image_list':image_list})
+                fileurl=settings.STATIC_URL+"images/"+file
+                exifdata = getexif(os.path.join(imgpath,file))
+                allimages.update({fileurl : exifdata})
+    print(allimages)
+    return render(request, 'gallery.html',context={'allimages':allimages})
+
+
+######################TODO######################
+#Get exif data from user upload files
+#Add Get exif data for own pics
+################################################
+
+#Function to get exif data
+def getexif(file):
+    tags_list = ['EXIF Aperture',
+                          'EXIF ExposureTime',
+                          'EXIF ISOSpeedRatings',
+                          'Image Model']
+
+    with open(file,'rb') as img:
+        tags = exifread.process_file(img)
+        exifvalues=[]
+        for tag in tags.keys():
+            if tag in tags_list:
+                exifvalues.append(str(tag)+" : "+str(tags[tag]))
+        return exifvalues
+    # print(exifdata)
